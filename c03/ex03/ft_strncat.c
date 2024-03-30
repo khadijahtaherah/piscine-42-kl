4fe0d3f1815513662d032b744b8e6b5e8ebc0e8d
Khadijah Taherah <k.taherah@gmail.com>
2024-03-30 13:59:16 +1000

    strncat function

---
+++ c03/ex03/ft_strncat.c
@@ -0,0 +1,35 @@
+char	*ft_strncat(char *dest, char *src, unsigned int nb)
+{
+	int	i;
+	int j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while ( j < (int)nb && src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+#include <stdio.h>
+#include <string.h>
+
+int	main(void)
+{
+	char src[50], dest[50];
+
+   	strcpy(src,  "Father 123");
+   	strcpy(dest, "I am your ");
+
+   	ft_strncat(dest, src, 20);
+
+	printf("result: %s\n", dest);
+	return (0);
+}
+*/
