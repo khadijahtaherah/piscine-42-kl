e1b7815555f6e08344668c72188e7362a9b8e128
Khadijah Taherah <k.taherah@gmail.com>
2024-03-30 19:12:34 +1000

    Updated

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -1,18 +1,29 @@
 char    *ft_strstr(char *str, char *to_find)
 {
-    int     i;
-    int     j;
-    char    *result = {0};
+    int i = 0;
+    int j = 0;
 
-    (void) to_find;
-    i = 0;
-    j = 0;
     while (str[i])
     {
-        j++;
+        if (str[i] == to_find[j])
+        {
+            // If the characters match, check if the whole substring matches
+            int temp_i = i;
+            while (str[temp_i] == to_find[j] && to_find[j])
+            {
+                temp_i++;
+                j++;
+            }
+            // If the whole substring matches, return the start of the substring
+            if (to_find[j] == '\0')
+                return &str[i];
+            // Reset j if the whole substring didn't match
+            j = 0;
+        }
         i++;
     }
-    return (result);
+    // Return NULL if the substring is not found
+    return (0);
 }
 
 #include <stdio.h>
@@ -20,7 +31,7 @@ char    *ft_strstr(char *str, char *to_find)
 
 int main(void)
 {
-    char *str = "I am your mother";
+    char *str = "I am your mother 123";
     char *to_find = "mother";
     char *result;
 
